5566d2e3f7c11ba63303c3211fa089d8fcc79a39
Mintimer <mintimer99@gmail.com>
2022-03-20 15:33:08 +0300

    initial commit

---
+++ SomeGitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeGitProject
+//
+//  Created by Минтимер Харасов on 20.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
